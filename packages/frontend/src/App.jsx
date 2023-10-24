ab836ec15bed00cf179ec86d12165c13855acbda
Jorrik Klijnsma <jorrikklijnsma@gmail.com>
2023-10-24 16:28:34 +0200

    init: setup backend and frontend

---
+++ packages/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css'; // Style it as needed
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Tire Change Notifier</h1>
+      </header>
+      <main>
+        {/* This is where the main content and logic will go */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
