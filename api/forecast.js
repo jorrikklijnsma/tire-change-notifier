911653912670a776d9bea4826086aa3aca9df974
Jorrik Klijnsma <jorrikklijnsma@gmail.com>
2023-10-26 20:22:20 +0200

    add: missing function

--- api/forecast.js
+++ api/forecast.js
@@ -101,3 +101,29 @@ function shouldChangeTires(weatherData) {
 		return 'No need to switch tires yet.';
 	}
 }
+
+async function getLastBotMessage() {
+	const endpoint = `${API_ENDPOINT}${API_KEY}/getUpdates`;
+
+	try {
+		const response = await axios.get(endpoint, { params: { offset: -1 } });
+		const data = response.data.result;
+
+		const messages = data
+			.filter((update) => update?.channel_post?.text && update?.channel_post?.chat?.id === parseInt(chatId))
+			.map((update) => {
+				const messageUnix = new Date(update.channel_post.date * 1000);
+				const dateOfToday = new Date();
+				const messageDate = `${messageUnix.getDate()}-${messageUnix.getMonth()}-${messageUnix.getFullYear()}`;
+				const dateOfTodayFormatted = `${dateOfToday.getDate()}-${dateOfToday.getMonth()}-${dateOfToday.getFullYear()}`;
+				const message = update.channel_post.text;
+				return { message, alreadySentToday: dateOfTodayFormatted === messageDate };
+			});
+
+		const latestMessage = messages[0];
+
+		return latestMessage;
+	} catch (error) {
+		console.error('Error fetching weather data:', error);
+	}
+}
